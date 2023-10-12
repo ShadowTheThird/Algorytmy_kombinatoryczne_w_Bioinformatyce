96f07dfaf294637a59aaaa98824e8cb0ea519e6d
Michal Walach <michal.walach111@gmail.com>
2023-10-12 12:58:48 +0200

    wczytywanie grafu z pliku

--- zad_II/main.cpp
+++ zad_II/main.cpp
@@ -3,12 +3,16 @@
 #include <vector>
 #define cout std::cout
 #define endl std::endl
+#define string std::string
 #define ifstream std::ifstream
 #define vector std::vector
 
 struct Node{
     int key;
     vector<int> neighbours;
+    Node(int _key){
+        key = _key;
+    }
 };
 
 vector<Node> graph;
@@ -19,5 +23,29 @@ int main(){
         cout << "file not found!";
         return 0;
     }
-    
+    string line;
+    for(int i = 0; getline(ifile, line); ++i){
+        string number = "";
+        int k = 0;
+        for(;isdigit(line[k]); ++k){
+            number += line[k];
+        }
+        graph.push_back(Node(stoi(number)));
+        for(; line[k] != '\0'; ++k){
+            if(isdigit(line[k])){
+                number = "";
+                for(; isdigit(line[k]); ++k){
+                    number += line[k];
+                }
+                graph[i].neighbours.push_back(stoi(number));
+            }
+        }
+    }
+    for(int i = 0; i < graph.size(); ++i){
+        cout << graph[i].key << ":\t";
+        for(int j = 0; j < graph[i].neighbours.size(); ++j){
+            cout << graph[i].neighbours[j] << '\t';
+        }
+        cout << endl;
+    }
 }
