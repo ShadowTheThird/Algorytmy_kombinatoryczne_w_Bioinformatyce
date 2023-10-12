1eff6dd21918ee7ce2539999190842c22fe1d6fc
Michal Walach <michal.walach111@gmail.com>
2023-10-12 12:25:54 +0200

    opening file

--- zad_II/main.cpp
+++ zad_II/main.cpp
@@ -2,7 +2,14 @@
 #include <iostream>
 #include <vector>
 #define cout std::cout
+#define endl std::endl
+#define ifstream std::ifstream
 
 int main(){
-
+    ifstream ifile("input.txt");
+    if(!ifile.is_open()){
+        cout << "file not found!";
+        return 0;
+    }
+    
 }
