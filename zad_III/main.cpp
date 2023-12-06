b9a789209ec5edb753455bb14248403aac0b380d
ShadowTheThird <michal.walach111@gmail.com>
2023-12-06 20:14:46 +0100

    no zmiany no

--- zad_III/main.cpp
+++ zad_III/main.cpp
@@ -25,16 +25,17 @@ struct Sequence{
             full_sequence = _sequence;
             return true;
         }
+        cout << "sequence overwrite attempt detected" << endl;
         return false;
     }
-    void Add_quality(int _quality){
-        quality.push_back(_quality);
+    void Add_quality(vector<short> _quality){
+        quality = _quality;
     }
     bool Quantity_check(){
         if(quality.size() == full_sequence.length()){
             return true;
         }
-        cout << "quality: " << quality.size() << "\tsequence: " << full_sequence.length();
+        cout << "quality: " << quality.size() << "\tsequence: " << full_sequence.length() << endl;
         return false; 
     }
     bool Asses_relevancy(int _minimal_quality){
@@ -53,61 +54,38 @@ struct Sequence{
 struct Motif{
     int position;
     string sequence = "";
+    Motif(string _sequence, int _position):sequence(_sequence),position(_position){}
+    Motif(){}
 };
 
 map<string, Sequence> sequences;
 map<string, map<string, vector<int>>> graph, temp;
 
-int Find_position(string _string){
-    for(int i = _string.length() - 1; i >= 0; --i){
-        if(isdigit(_string[i])){
-            return i;
-        }
-    }
-    return _string.length();
-}
-
-Motif Search_first(){
-    Motif motif;
-    int lowest = graph.begin()->second.begin()->second[0];
-    string sequence = graph.begin()->first;
-    for(auto it_1 = graph.begin(); it_1 != graph.end(); ++it_1){
-        for(auto it_2 = it_1->second.begin(); it_2 != it_1->second.end(); ++it_2){
-            for(int i = 0; i < it_2->second.size(); ++i){
-                if(it_2->second[i] < lowest){
-                    lowest = it_2->second[i];
-                    sequence = it_1->first;
-                }
-            }
-        }
-    }
-    motif.position = lowest;
-    motif.sequence = sequence;
-    return motif;
-}
-
 int main(){
-    string fasta = "fasta\\sequence_0.fasta", qual = "qual\\sequence_0.qual", fasta_line_1 = "", fasta_line_2 = "", qual_line_1 = "", qual_line_2 = "";
-    ifstream fasta_file, qual_file;
-    for(int i = 1; i <= 5; ++i){
-        fasta[Find_position(fasta)] = i+'0', qual[Find_position(qual)] = i+'0';
-        fasta_file.open(fasta), qual_file.open(qual);
-        getline(fasta_file, fasta_line_1), getline(qual_file, qual_line_1);
-        getline(fasta_file, fasta_line_2), getline(qual_file, qual_line_2);
-        if(!sequences[fasta_line_1].Add_sequence(fasta_line_2)){
-            cout << "process failed due to full_sequence overwrite attempt";
-            return 0;
-        }
-        for(int label_length = 0; label_length < qual_line_2.length(); ++label_length){
-            string number = "";
-            for(;isdigit(qual_line_2[label_length]); ++label_length){
-                number += qual_line_2[label_length];
+    ifstream ifile("input.txt");
+    string line;
+    while(getline(ifile, line)){
+        if(line[0] == '>'){
+            string sequence, quality;
+            getline(ifile, sequence);
+            getline(ifile, quality);
+            if(!sequences[line].Add_sequence(sequence)){
+                cout << "ERROR" << endl;
+                return 0;
             }
-            if(number != ""){
-                sequences[qual_line_1].Add_quality(stoi(number));
+            vector<short> temp_qual;
+            for(int i = 0; i < quality.size(); ++i){
+                string number = "";
+                for(; quality[i] != ' '; ++i){
+                    number += quality[i];
+                }
+                temp_qual.push_back(stoi(number));
             }
+            sequences[line].Add_quality(temp_qual);
         }
-        fasta_file.close(), qual_file.close();
+        else{
+            cout << "ERROR\nincorrect file format detected" << endl;
+        }
     }
     int label_length = 5, relevant_quality = 20;
     cout << "input relevant quality threshold:\t";
@@ -128,6 +106,10 @@ int main(){
                 return 0;
             }
         }
+        else{
+            cout << "ERROR\nmissmatched sequence length and quality count";
+            return 0;
+        }
     }
     for(auto it_1 = sequences.begin(); it_1 != sequences.end(); ++it_1){
         Motif *label = new Motif[label_length];
@@ -140,8 +122,9 @@ int main(){
                 }
             }
             else{
-                graph[label[i - (label_length*(i/label_length))].sequence][it_1->first].push_back(label[i - (label_length*(i/label_length))].position);
-                label[i - (label_length*(i/label_length))].sequence = "", label[i - (label_length*(i/label_length))].position = it_2->first;
+                int temp = i - (label_length*(i/label_length));
+                graph[label[temp].sequence][it_1->first].push_back(label[temp].position);
+                label[temp].sequence = "", label[temp].position = it_2->first;
                 for(int j = 0; j < label_length; ++j){
                     label[j].sequence += it_2->second;
                 }
@@ -156,8 +139,8 @@ int main(){
     graph.clear();
     graph = temp;
     temp.clear();
-    while(graph.size()){
-        Motif potential_motif = Search_first();
+    for(auto it = graph.begin(); it != graph.end(); ++it){
+        Motif potential_motif(it->first, it->second.begin()->second[0]);
         bool within_range = true;
         for(auto it = graph[potential_motif.sequence].begin(); it != graph[potential_motif.sequence].end(); ++it){
             if(it->second.size() > 1){
--- zad_III/sequence_generator/sequence_generator.cpp
+++ zad_III/sequence_generator/sequence_generator.cpp
@@ -1,72 +1,100 @@
 #include <ctime>
 #include <fstream>
 #include <iostream>
+#include <set>
 #include <string>
+#include <utility>
 #include <vector>
 #define cout std::cout
 #define endl std::endl;
 #define ifstream std::ifstream
 #define ofstream std::ofstream
+#define pair std::pair
 #define string std::string
 #define vector std::vector
 
-
-
 int main(){
     srand(time(NULL));
-    ifstream fasta_file("input.txt");
-    string file_name, sequence, qual, changed_sequence;
-    int random_value;
-    vector<int> changed_qual;
-    if(!fasta_file.is_open()){
-        cout << "couldn't open file" << endl;
-        return 0;
-    }
-    getline(fasta_file, file_name);
-    getline(fasta_file, sequence);
-    getline(fasta_file, qual);
-    while(true){
-        random_value = rand()%sequence.length();
-        if(random_value > 10){
-            break;
+    ifstream qual("qual.txt"), fasta("fasta.txt");
+    string qual_line = "", fasta_line = "";
+    std::set<int> chosen;
+    for(; chosen.size() < 5;){
+        int random = rand()%500;
+        if(chosen.find(random) == chosen.end()){
+            chosen.insert(random);
         }
     }
-    for(int i = 0; i < qual.length(); ++i){
-        if(changed_qual.size() == random_value){
-            for(int i = 0; i < 9; ++i){
-                changed_qual.push_back(99);
+    auto it = chosen.begin();
+    ofstream ofile("output.txt");
+    vector<pair<string,pair<string,string>>> format;
+    getline(fasta, fasta_line), getline(qual, qual_line);
+    for(int sequence = 0; it != chosen.end(); ++sequence){
+        if(sequence == *it){
+            string name = fasta_line;
+            string sequence = "", quality = "";
+            while(getline(fasta, fasta_line)){
+                if(fasta_line[0] == '>'){
+                    break;
+                }
+                sequence += fasta_line;
+            }
+            while(getline(qual, qual_line)){
+                if(qual_line[0] == '>'){
+                    break;
+                }
+                if(quality != ""){
+                    quality += " ";
+                }
+                quality += qual_line;
+            }
+            format.push_back(std::make_pair(name, std::make_pair(sequence,quality)));
+            ++it;
+        }
+        else{
+            while(getline(fasta, fasta_line)){
+                if(fasta_line[0] == '>'){
+                    break;
+                }
+            }
+            while(getline(qual, qual_line)){
+                if(qual_line[0] == '>'){
+                    break;
+                }
             }
         }
-        if(isdigit(qual[i])){
-            string number = "";
-            for(; isdigit(qual[i]); ++i){
-                number += qual[i];
+    }
+    int id = 0, shortest = format[0].second.first.length();
+    for(int i = 1; i < format.size(); ++i){
+        if(format[i].second.first.length() < shortest){
+            id = i;
+            shortest = format[i].second.first.length();
+        }
+    }
+    int random = rand()%(format[id].second.first.length() - 40) + 31;
+    for(int i = 0; i < 5; ++i){
+        ofile << format[i].first << endl;
+        int offset = rand()%81 - 40;
+        while(random + offset + 9 > format[i].second.first.length()){
+            offset = rand()%81 - 40;
+        }
+        string sequence = format[i].second.first.substr(0, random + offset), quality;
+        sequence += "TTTTTTTTT";
+        sequence += format[i].second.first.substr(random + offset + 9);
+        ofile << sequence << endl;
+        int k = 0;
+        for(int j = 0; j < random + offset; ++j, ++k){
+            while(format[i].second.second[k] != ' '){
+                ++k;
             }
-            changed_qual.push_back(stoi(number));
         }
+        quality = format[i].second.second.substr(0, k);
+        for(int j = 0; j < 9; ++j, ++k){
+            while(format[i].second.second[k] != ' '){
+                ++k;
+            }
+            quality += "99 ";
+        }
+        quality += format[i].second.second.substr(k);
+        ofile << quality << endl;
     }
-    changed_sequence = sequence.substr(0,random_value);
-    changed_sequence += "AACCCCAAT";
-    changed_sequence += sequence.substr(random_value);
-    fasta_file.close();
-    if(changed_qual.size() == changed_sequence.size()){
-        ofstream fasta_file("output.txt");
-        int number_start = file_name.find("length=") + 7;
-        string number = "";
-        while(isdigit(file_name[number_start])){
-            number += file_name[number_start];
-            ++number_start;
-        }
-        int x = number_start - number.length();
-        number = std::to_string(stoi(number) + 9);
-        for(int i = 0; i < number.length(); ++i){
-            file_name[x + i] = number[i];
-        }
-        fasta_file << file_name << endl;
-        fasta_file << changed_sequence << endl;
-        for(int i = 0; i < changed_qual.size(); ++i){
-            fasta_file << changed_qual[i] << " ";
-        }
-    }
-    return 0;
 }
